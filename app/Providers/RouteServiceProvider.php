611a3c54f7a1a78ff8b9239bb224fdbc61b89c3d
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-10-30 20:28:47 -0600

    rutas de api vehiculos definidas en "./routes/vehiculo_api.php"

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,5 +36,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        // vehiculo api routes
+        Route::middleware("api")
+            ->prefix("api/vehiculos")
+            ->group(base_path("routes/vehiculo_api.php"));
     }
 }
